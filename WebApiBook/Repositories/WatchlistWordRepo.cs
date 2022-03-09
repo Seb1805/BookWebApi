e7ab72171300992eac217b84172d86334417a639
Sebastian <sebastian.clausen@hotmail.com>
2022-03-09 10:51:47 +0100

    First commit

---
+++ WebApiBook/Repositories/WatchlistWordRepo.cs
@@ -0,0 +1,20 @@
+﻿using BookApi.Models;
+using WebApiBook.Data;
+using WebApiBook.Repositories.Interfaces;
+
+namespace WebApiBook.Repositories
+{
+    public class WatchlistWordRepo : IWatchlistWord
+    {
+        private readonly BookContext _context;
+
+        public WatchlistWordRepo(BookContext context)
+        {
+            _context = context;
+        }
+        public void AddWatchlistWord(WatchlistWord word)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
