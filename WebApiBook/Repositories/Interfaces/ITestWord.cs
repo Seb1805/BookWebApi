214a08492bbd36b096e50d6d553ec60cd9c60a7f
Sebastian <sebastian.clausen@hotmail.com>
2022-03-09 21:10:33 +0100

    TestWord interface and repo

---
+++ WebApiBook/Repositories/Interfaces/ITestWord.cs
@@ -0,0 +1,9 @@
+﻿using WebApiBook.Models;
+
+namespace WebApiBook.Repositories.Interfaces
+{
+    public interface ITestWord
+    {
+        public IEnumerable<TestWord> GetAllTestWords();
+    }
+}
---
+++ WebApiBook/Repositories/TestWordRepo.cs
@@ -0,0 +1,22 @@
+﻿using WebApiBook.Data;
+using WebApiBook.Models;
+using WebApiBook.Repositories.Interfaces;
+
+namespace WebApiBook.Repositories
+{
+    public class TestWordRepo : ITestWord
+    {
+        private readonly BookContext _context;
+
+        public TestWordRepo(BookContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<TestWord> GetAllTestWords()
+        {
+            var list = _context.TestWordList.ToList();
+            return list;
+        }
+    }
+}
